ad1b345470249e1370ae22a83fd422fed9d8e6ea
nt37vde <nt37@village-emploi.fr>
2026-03-31 14:08:05 +0200

    je crée le conflit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // Fonction pour choisir le type de jeu
-function choisirPhraseOuMots(){
+function choisirPhraseOuMots() {
     let choix = ""
-    while (choix !== "mots" && choix !== "phrases"){
-        choix = prompt("Voulez-vous jouer avec la liste de mots ou la liste de phrases ?")       
-    }   
+    while (choix !== "mots" && choix !== "phrases") {
+        choix = prompt("Voulez-vous jouer avec la liste de mots ou la liste de phrases ? (tapez 'mots' ou 'phrases')")
+        if (choix !== "mots" && choix !== "phrases") {
+            alert("Veuillez entrer 'mots' ou 'phrases' uniquement.")
+        }
+    }
     return choix
 }
